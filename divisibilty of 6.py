88bc0ed44ab2792df9fe71a6ff729029a45af608
Vorch135 <106906243+Vorch135@users.noreply.github.com>
2023-01-31 17:08:46 +0530

    Checking if the number is divisible by 6.

---
+++ divisibilty of 6.py
@@ -0,0 +1,19 @@
+x = 10946
+y = 1065
+z = 675
+
+#Check if number is divible by 6
+if x % 3 == 0 and x % 2 == 0:
+    print ('yes!')
+else:
+    print('no!')
+
+if y % 3 == 0 and y % 2 == 0:
+    print ('yes!')
+else:
+    print('no!')
+
+if z % 3 == 0 and z % 2 == 0:
+    print ('yes!')
+else:
+    print('no!')
