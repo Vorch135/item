ce640fe812d7daedf142fa4780035287398c0b23
Vorch135 <106906243+Vorch135@users.noreply.github.com>
2023-01-31 16:58:29 +0530

    location

---
+++ random test.py
@@ -0,0 +1,11 @@
+import pygame
+x = 'Restaurant'
+ 
+if x == 'Bank':
+    print('I love money')
+elif x == 'Auto Shop':
+    print('Car are cool!')
+elif x == 'Restaurant':
+    print('I love food!')
+else:
+    print('lets go somewhere else...')
