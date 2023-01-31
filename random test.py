fd9376625497e025549039141890b204bce9e243
Vorch135 <106906243+Vorch135@users.noreply.github.com>
2023-01-31 17:39:50 +0530

    random test.py

--- random test.py
+++ random test.py
@@ -1,11 +1,13 @@
-import pygame
-x = 'Restaurant'
- 
-if x == 'Bank':
+import random
+
+x_list = ['Restaurant', 'Hotel', 'Bank', 'Auto Shop', 'Park', 'Hospital']
+x_random_location = random.choice(x_list)
+
+if x_random_location == 'Bank':
     print('I love money')
-elif x == 'Auto Shop':
-    print('Car are cool!')
-elif x == 'Restaurant':
+elif x_random_location == 'Auto Shop':
+    print('Cars are cool!')
+elif x_random_location == 'Restaurant':
     print('I love food!')
 else:
     print('lets go somewhere else...')
